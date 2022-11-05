1e85d00659391c9cd835decbbb247c0fcab615ba
SaudSaad <sdswd5029@gmail.com>
2022-11-05 12:27:46 +0300

    creat custom text fild .

--- lib/widget/add_note_bottom_sheet.dart
+++ lib/widget/add_note_bottom_sheet.dart
@@ -5,6 +5,28 @@ class AddNoteBottomsheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: [
+          const SizedBox(height: 32),
+          TextField(
+            cursorColor: Colors.green[300],
+            decoration: InputDecoration(
+                hintText: 'Title',
+                border: buildBorder(),
+                enabledBorder: buildBorder(),
+                focusedBorder: buildBorder(Colors.green[300])),
+          )
+        ],
+      ),
+    );
+  }
+
+  OutlineInputBorder buildBorder([color]) {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(8),
+      borderSide: BorderSide(color: color ?? Colors.white),
+    );
   }
 }
